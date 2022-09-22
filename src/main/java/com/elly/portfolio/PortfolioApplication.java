3122821dea657d9805a02b693347325b935552cc
elly <limsuhyeon8040@gmail.com>
2022-09-22 20:04:18 +0900

    프로젝트 정리

--- src/main/java/com/elly/portfolio/PortfolioApplication.java
+++ src/main/java/com/elly/portfolio/PortfolioApplication.java
@@ -2,8 +2,9 @@ package com.elly.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
